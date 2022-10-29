9716f24303a235e4d64aa38d410f114489d3b74a
p.kansal.80lpa@gmail.com <p.kansal.80lpa@gmail.com>
2022-10-30 04:08:01 +0530

    Posting with Postman

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// handing all parsing
+app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
     var email = req.body.email;
