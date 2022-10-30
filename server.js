1a39258abaef6cb79433b49742517154965f7ab9
p.kansal.80lpa@gmail.com <p.kansal.80lpa@gmail.com>
2022-10-30 06:26:18 +0530

    Getting Data Back

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const {save_user_information} = require('./models/server_db');
+const {save_user_information, get_total_amount} = require('./models/server_db');
 
 // handing all parsing
 app.use(bodyParser.json());
@@ -19,6 +19,11 @@ app.post('/', async (req, res) => {
     var result = await save_user_information({"amount": amount, "email": email});
     res.send(result);
 })
+
+app.get('/get_total_amount', async (req, res) => {
+    var result = await get_total_amount();
+    res.send(result);
+});
 
 app.listen(3000, () => {
     console.log('Hearing on port 3000');
