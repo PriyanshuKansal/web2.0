e18401855e805188208c060e6ec6392adbffa12e
p.kansal.80lpa@gmail.com <p.kansal.80lpa@gmail.com>
2022-11-04 15:12:03 +0530

    Creating Payment with Paypal

--- server.js
+++ server.js
@@ -17,18 +17,107 @@ paypal.configure({
     'client_secret': 'EFYXv1A1b6vgq42uxKmtAir2GxmiTYa68FVj0HZ1Jqu8soXNw92ptcivFbQ2AYDUNwbGxJJjE0oySjTx'
 })
 
-app.post('/post_info', async (req, res) => {
-    var email = req.body.email;
-    var amount = req.body.amount;
+// app.post('/post_info', async (req, res) => {
+//     var email = req.body.email;
+//     var amount = req.body.amount;
 
-    if (amount <= 1) {
-        return_info = {};
-        return_info.error = true;
-        return_info.message = "Amount should be greater than 1";
-        res.send(return_info);
+//     if (amount <= 1) {
+//         return_info = {};
+//         return_info.error = true;
+//         return_info.message = "Amount should be greater than 1";
+//         res.send(return_info);
+//     }
+//     var result = await save_user_information({"amount": amount, "email": email});
+
+//     var create_payment_json = {
+//         "intent": "sale",
+//         "payer": {
+//             "payment_method": "paypal"
+//         },
+//         "redirect_urls": {
+//             "return_url": "http://localhost:3000/success",
+//             "cancel_url": "http://localhost:3000/cancel"
+//         },
+//         "transactions": [{
+//             "item_list": {
+//                 "items": [{
+//                     "name": "Lottery",
+//                     "sku": "Funding",
+//                     "price": "amount",
+//                     "currency": "USD",
+//                     "quantity": 1
+//                 }]
+//             },
+//             "amount": {
+//                 "currency": "USD",
+//                 "total": "amount"
+//             },
+//             'payee': {
+//                 'email': 'lottery__manager@lotteryapp.com'
+//             },
+//             "description": "Lottery Purchase"
+//         }]
+//     };
+
+    app.post('/post_info', async (req,res)=>{
+        var email = req.body.email;
+        var amount = req.body.amount;
+      
+        if(amount <= 1){
+          return_info = {};
+          return_info.error = true;
+          return_info.message = "The amount should be greater than 1";
+          return res.send(return_info);
+        }
+        // var fee_amount = amount * 0.9;
+        var result = await save_user_information({"amount" : amount, "email" : email});
+        // req.session.paypal_amount = amount;
+        var create_payment_json = {
+          "intent": "sale",
+          "payer": {
+              "payment_method": "paypal"
+          },
+          "redirect_urls": {
+              "return_url": "http://localhost:3000/success",
+              "cancel_url": "http://localhost:3000/cancel"
+          },
+          "transactions": [{
+              "item_list": {
+                  "items": [{
+                      "name": "Lottery",
+                      "sku": "Funding",
+                      "price": amount,
+                      "currency": "USD",
+                      "quantity": 1
+                  }]
+              },
+              "amount": {
+                  "currency": "USD",
+                  "total": amount
+              },
+              'payee' : {
+                'email' : 'lottery__manager@lotteryapp.com'
+              },
+              "description": "Lottery purchase"
+          }]
+      };
+    
+    
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+        throw error;
+    } else {
+        console.log("Create Payment Response");
+        console.log(payment);
+        for(var i = 0; i< payment.links.length; i++){
+          if(payment.links[i].rel =='approval_url'){
+            return res.send(payment.links[i].href);
+          }
+        }
     }
-    var result = await save_user_information({"amount": amount, "email": email});
-    res.send(result);
+    });
+
+    
 })
 
 app.get('/get_total_amount', async (req, res) => {
