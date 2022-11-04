e72c752279c3a68399d3e734bb791feef119f415
p.kansal.80lpa@gmail.com <p.kansal.80lpa@gmail.com>
2022-11-04 10:43:45 +0530

    Installing Paypal SDK

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const bodyParser = require('body-parser');
 const {save_user_information, get_total_amount} = require('./models/server_db');
 const path = require('path');
 const publicPath = path.join(__dirname, './public');
+const paypal = require('paypal-rest-sdk');
 
 // handing all parsing
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
+
+// Paypal Configuration 
+paypal.configure({
+    'mode': 'sandbox',
+    'client_id': 'AbOh-Q_k9RN8BmHhjBg_MH52_r4z9UAjs2JKggC-PTZ4D8O0ssFqxoJQDNrGRCpS5uMgIYIKTz5JUaMJ',
+    'client_secret': 'EFYXv1A1b6vgq42uxKmtAir2GxmiTYa68FVj0HZ1Jqu8soXNw92ptcivFbQ2AYDUNwbGxJJjE0oySjTx'
+})
 
 app.post('/post_info', async (req, res) => {
     var email = req.body.email;
